02e188ae09e0c70bfecf707ef56f5071debeb789
yeonjae02 <0yeonjae2@naver.com>
2022-09-24 02:46:02 +0900

    Lab02

---
+++ 2w-1.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// น้มุ 3273
+
+int compare(const void *a, const void *b);
+int binarySearch(int arr[], int n, int find);
+
+int main()
+{
+	int n, result, count=0;
+	scanf("%d", &n);
+
+	int* arr = (int*)malloc(sizeof(int) * n);
+	for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+	qsort(arr, n, sizeof(int), compare);
+	scanf("%d", &result);
+
+	for (int i = 0; i < n; i++)
+	{
+		int find = result - arr[i];
+		if (binarySearch(arr, n, find) != -1) count++;
+	}
+
+	printf("%d", count/2);
+	return 0;
+}
+
+int compare(const void* a, const void* b)
+{
+	int n1 = *(int*)a;
+	int n2 = *(int*)b;
+
+	if (n1 < n2) return -1;
+	else if (n1 > n2) return 1;
+	else return 0;
+}
+
+int binarySearch(int arr[], int n, int find)
+{
+	int low = 0, high=n-1, mid;
+	while (low <= high)
+	{
+		mid = (low + high) / 2;
+		if (find == arr[mid]) return mid;
+		else if (find < arr[mid]) high = mid - 1;
+		else if (find > arr[mid]) low = mid + 1;
+	}
+	return -1;
+}
---
+++ 2w-2.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <string.h>
+
+// น้มุ 10866
+
+int deque[10001] = {0,};
+
+int main()
+{
+	int n, input, count = 0;
+	char order[12];
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%s", &order);
+		if (!strcmp(order, "push_front"))
+		{
+			count++;
+			for (int i = count; i > 0; i--)
+			{
+				deque[i] = deque[i-1];
+			}
+			scanf("%d", &input);
+			deque[0] = input;
+		}
+		else if (!strcmp(order, "push_back"))
+		{
+			count++;
+			scanf("%d", &input);
+			deque[count - 1] = input;
+		}
+		else if (!strcmp(order, "pop_front"))
+		{
+			if (count == 0) printf("-1\n");
+			else
+			{
+				printf("%d\n", deque[0]);
+				deque[0] = 0;
+				for (int i = 0; i < count; i++) deque[i] = deque[i + 1];
+				count--;
+			}
+		}
+		else if (!strcmp(order, "pop_back"))
+		{
+			if (count == 0) printf("-1\n");
+			else
+			{
+				printf("%d\n", deque[count - 1]);
+				deque[count - 1] = 0;
+				count--;
+			}
+		}
+		else if (!strcmp(order, "size"))
+		{
+			printf("%d\n", count);
+		}
+		else if (!strcmp(order, "empty"))
+		{
+			if (count == 0) printf("1\n");
+			else printf("0\n");
+		}
+		else if (!strcmp(order, "front"))
+		{
+			if (count == 0) printf("-1\n");
+			else printf("%d\n", deque[0]);
+		}
+		else if (!strcmp(order, "back"))
+		{
+			if (count == 0) printf("-1\n");
+			else printf("%d\n", deque[count - 1]);
+		}
+	}
+	return 0;
+}
